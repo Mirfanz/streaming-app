0507d8024a18714cc06ba96f133fdbc568a56368
fandevid <radenirfan378@gmail.com>
2022-07-12 16:31:37 +0700

    Deploy

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -9,12 +9,15 @@ use CodeIgniter\Database\BaseUtils;
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="<?= base_url('/library/boxicons/css/boxicons.min.css'); ?>" />
-    <link rel="stylesheet" href="<?= base_url('/library/bootstrap/dist/css/bootstrap.min.css'); ?>" />
+    <!-- Bootstrap -->
+    <!-- <link rel="stylesheet" href="<?= base_url('/library/bootstrap/dist/css/bootstrap.min.css'); ?>" /> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
+    
     <link rel="stylesheet" href="<?= base_url('/library/swiperjs/swiper.css'); ?>" />
 
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap">
     <link rel="stylesheet" href="<?= base_url('/css/style.css'); ?>" />
     <link rel="shortcut icon" href="<?= base_url('favicon.ico'); ?>" type="image/x-icon" />
     <?= $this->
@@ -100,7 +103,8 @@ use CodeIgniter\Database\BaseUtils;
 
     <script src="<?= base_url('/library/swiperjs/swiper.js'); ?>"></script>
     <script src="<?= base_url('/library/sweetalert/sweetalert.js'); ?>"></script>
-    <script src="<?= base_url('/library/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+    <!-- <script src="<?= base_url('/library/bootstrap/dist/js/bootstrap.min.js'); ?>"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?= base_url('/library/jquery/jquery.js'); ?>"></script>
     <script src="<?= base_url('/js/script.js'); ?>"></script>
 
